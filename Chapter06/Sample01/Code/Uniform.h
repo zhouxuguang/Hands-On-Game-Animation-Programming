b6003869425f69241a1e1c2f98b120d66200e0f8
zhouxuguang <zhouxuguang236@126.com>
2023-08-20 11:29:28 +0800

    修复黑色 & 增加错误检查的函数

--- Chapter06/Sample01/Code/Uniform.h
+++ Chapter06/Sample01/Code/Uniform.h
@@ -33,6 +33,19 @@ public:
     }
 };
 
+#define UNIFORM_IMPL(gl_func, tType, dType) \
+template<> \
+void Uniform<tType>::Set(unsigned int slot, tType* data, unsigned int length) { \
+    gl_func(slot, (GLsizei)length, (dType*)&data[0]); \
+}
 
+UNIFORM_IMPL(glUniform1iv, int, int)
+UNIFORM_IMPL(glUniform4iv, ivec4, int)
+UNIFORM_IMPL(glUniform2iv, ivec2, int)
+UNIFORM_IMPL(glUniform1fv, float, float)
+UNIFORM_IMPL(glUniform2fv, vec2, float)
+UNIFORM_IMPL(glUniform3fv, vec3, float)
+UNIFORM_IMPL(glUniform4fv, vec4, float)
+UNIFORM_IMPL(glUniform4fv, quat, float)
 
 #endif
